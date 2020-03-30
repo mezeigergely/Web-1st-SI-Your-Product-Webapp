74b5152f6213bf7a04c2300aae4768bc59041ecc
Gergely Mezei <mezei.gergely89@gmail.com>
2020-03-30 16:48:13 +0200

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace project.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        public void OnPost([FromForm(Name = "mail")]string mail)
+        {
+            if (!SubscribeModel.subscribers.Contains(mail))
+            {
+                SubscribeModel.subscribers.Add(mail);
+            }
+            Response.Redirect("Subscribe");
+        }
+    }
+}
---
+++ Pages/Tech_Specs.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace project.Pages
+{
+    public class Tech_SpecsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
